9a21f7f1fbeb750134ea9e9c8e62ebb74384e562
Bertrand <bithurburn@gmail.com>
2015-07-08 20:09:27 -0700

    Made a canvas and can now add rectangles to it.

--- src/js/element.js
+++ src/js/element.js
@@ -1,31 +1,55 @@
-$(function() {
-    $( "#element" ).draggable();
-});
+var palette = {
+    current: "select",
+    options: document.getElementsByClassName("option"),
+    highlight: function() {
+        // White out the background of every option
+        for(i = 0; i < this.options.length; i++) {
+            this.options[i].style.backgroundColor = "#FFFFFF";
+        }
 
-$(function() {
-    $( "#element" ).resizable();
-});
-
-function highlight() {
-    var options = document.getElementsByClassName("option");
-
-    for(i = 0; i < options.length; i++) {
-        options[i].style.backgroundColor = "#FFFFFF";
+       event.target.style.backgroundColor = "#AAFFFF";
+       this.current = event.target.id;
     }
-
-   event.target.style.backgroundColor = "#AAFFFF";
 }
 
-/*$(function () {
-    // Creates canvas 320 x 200 at 10, 50
-    var paper = Raphael(10, 50, 320, 200);
-    //paper.resizable();
+var canvas = {
+    paper: null,
+    makeCanvas: function() {
+        var container = document.getElementById("canvas");
+        this.paper = Raphael(container, 1024, 720);
+    },
+    canvasClick: function() {
+        var x = event.clientX;
+        var y = event.clientY;
 
-    // Creates circle at x = 50, y = 40, with radius 10
-    var circle = paper.circle(50, 40, 10);
-    // Sets the fill attribute of the circle to red (#f00)
-    circle.attr("fill", "#f00");
-
-    // Sets the stroke attribute of the circle to white
-    circle.attr("stroke", "#fff");
-});*/
+        // Check for the current palette selection
+        switch(palette.current) {
+            case "select":
+              break;
+            case "comment":
+              break;
+            case "decomposition":
+              break;
+            case "contribution":
+              break;
+            case "dependency":
+              break;
+            case "belief_link":
+              break;
+            case "actor":
+              break;
+            case "softgoal":
+              break;
+            case "goal":
+              goal = this.paper.rect(x, y, 100, 100);
+              //this.paper.freeTransform(goal);
+              break;
+            case "task":
+              break;
+            case "resource":
+              break;
+            case "belief":
+              break;
+        }
+    }
+}
